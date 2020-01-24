4772cf26471739ce23c561fc517b4915249a92c4
RanneKapcie <kurasz.leszek@gmail.com>
2020-01-24 15:19:29 +0100

    fixed secret import

---
+++ nofences/secret.py
@@ -0,0 +1,2 @@
+SECRET_KEY = '$=$=f)q2l(453r+n!29=zw2-6)4y+nesl@+=ya8-%^&mw*sp1+'
+db_password = 'postgres'
--- nofences/settings.py
+++ nofences/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+from .secret import *
 import os
-from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
